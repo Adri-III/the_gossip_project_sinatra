3f361ab1ea47edadbbbaa100cd4d727080abdf27
Adri-III <trrs.adrien@gmail.com>
2021-05-03 22:12:39 +0200

    Projet Validant - Ruby: Gossip_Project_Sinatra

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+	ruby '2.7.1'
+	gem 'rubocop'
+	gem 'pry'
+  gem 'sinatra'
+  gem 'shotgun'
---
+++ lib/gossip.rb
@@ -0,0 +1,46 @@
+require 'csv'
+
+class Gossip
+  attr_accessor :author, :content
+
+  def initialize(author, content)
+    @author = author
+    @content = content
+  end
+
+  def save
+    CSV.open("./db/gossip.csv", "ab") do |csv|
+      csv << [@author, @content]
+    end
+  end
+
+  def self.all
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    return all_gossips
+  end
+
+  def self.find(index)
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    return all_gossips[index.to_i]
+  end
+
+  def self.update(index, author, content)
+    array_updated = CSV.read("./db/gossip.csv")
+
+    array_updated[index.to_i][0] = author
+    array_updated[index.to_i][1] = content
+
+    CSV.open("./db/gossip.csv","w+") do |csv|
+        array_updated.each do |line_updated|  
+           csv << line_updated
+          end
+      end
+  end
+
+end
